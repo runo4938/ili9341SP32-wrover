3f9ce283f61beb63731608eb98bdad3c730c14cf
runo4938 <run04938@gmail.com>
2025-09-30 10:07:19 +0300

    654

--- src/main.cpp
+++ src/main.cpp
@@ -1361,6 +1361,23 @@ void audio_eof_speech(const char *info)
 }
 */
 // для старого меню
+// void listStaton()
+// {
+//   String partlistStation;
+//   uint8_t i = 0;
+//   while (i <= numbStations)
+//   {
+//     int ind_to_tab = StationList[i].indexOf('\t');
+//     String nameStat = StationList[i].substring(0, ind_to_tab);
+
+//     String newStat = StationList[i].substring(ind_to_tab + 1, StationList[i].length());
+
+//     partlistStation += String("<tr><td>") + String(i) + String("</td>") + String("<td>") + nameStat + String("</td>") + String("<td>") + newStat + String("</td></tr>");
+//     listRadio = String("<table class=\"table table-success table-striped\"> <thead><tr><th>№</th><th>Station name</th><th>Station url</th></tr></thead><tbody>") + partlistStation + String("</tbody></table>");
+//     i++;
+//   }
+// }
+
 void listStaton()
 {
   String partlistStation;
@@ -1369,13 +1386,18 @@ void listStaton()
   {
     int ind_to_tab = StationList[i].indexOf('\t');
     String nameStat = StationList[i].substring(0, ind_to_tab);
-
     String newStat = StationList[i].substring(ind_to_tab + 1, StationList[i].length());
 
-    partlistStation += String("<tr><td>") + String(i) + String("</td>") + String("<td>") + nameStat + String("</td>") + String("<td>") + newStat + String("</td></tr>");
-    listRadio = String("<table class=\"table table-success table-striped\"> <thead><tr><th>№</th><th>Station name</th><th>Station url</th></tr></thead><tbody>") + partlistStation + String("</tbody></table>");
-    i++;
+    partlistStation += String("<tr><td class='index-cell'>") + String(i) + String("</td>") + 
+                      String("<td class='name-cell'>") + nameStat + String("</td>") + 
+                      String("<td class='url-cell'><span class='url-text'>") + newStat + String("</span></td></tr>");
+    
+    listRadio = String("<div class='dark-table-wrapper'><table class='dark-table'><thead><tr><th>#</th><th>STATION</th><th>URL</th></tr></thead><tbody>") + 
+                partlistStation + String("</tbody></table></div>");
+     i++;
   }
+  
+
 }
 
 String processor(const String &var)
